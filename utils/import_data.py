8872e4a32f57536fcb1a983dc9e26c376928d834
Omid Najaee <o.najaee@gmail.com>
2019-06-11 09:54:10 +0200

    add plan.md

--- utils/import_data.py
+++ utils/import_data.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pn
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import normalize
 
 FILE_PATH = './clean_data.csv';
 TRAIN_TO_TEST_RATIO = 0.8
@@ -9,11 +8,20 @@ TRAIN_FILE_NAME = './data/train.csv'
 TEST_FILE_NAME = './data/test.csv'
 CORR_FILE_NAME = './data/corr.png'
 
+def norm(df):
+    result = df.copy()
+    for feature_name in df.columns:
+        max_value = df[feature_name].max()
+        min_value = df[feature_name].min()
+        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+    return result
+
 data = pn.read_csv(FILE_PATH)
 selection = np.random.randn(len(data)) < TRAIN_TO_TEST_RATIO
 
-train = normalize(data[selection])
-test = normalize(data[~selection])
+
+train = norm(data[selection])
+test = norm(data[~selection])
 
 np.savetxt(TRAIN_FILE_NAME, train, delimiter=',')
 np.savetxt(TEST_FILE_NAME, test, delimiter=',');
